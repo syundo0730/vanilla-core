7f4c62dfc84e77e6fc26bcb32fd606443596ba97
syundo0730 <syundo0730@gmail.com>
2017-08-22 00:11:40 +0900

    fix test case

--- central/src/api/serial/servo/futaba/RSApi.h
+++ central/src/api/serial/servo/futaba/RSApi.h
@@ -8,16 +8,16 @@ class SerialApi;
 
 class RSApi
 {
-  public:
-	static std::unique_ptr<RSApi> instantiate(SerialApi &serialApi);
-	virtual void sendMultiPosition(const std::vector<uint8_t> &ids, const std::vector<uint16_t> &pos) = 0;
-	virtual void sendPosition(uint8_t id, uint16_t pos) = 0;
-	virtual void on() = 0;
-	virtual void off() = 0;
-    virtual void setID(uint8_t currentID, uint8_t targetID) = 0;
-    virtual void setAllID(uint8_t targetID) = 0;
-    virtual void writeToROM(uint8_t id) = 0;
-    virtual void reboot(uint8_t id) = 0;
+public:
+  static std::unique_ptr<RSApi> instantiate(SerialApi &serialApi);
+  virtual void sendMultiPosition(const std::vector<uint8_t> &ids, const std::vector<uint16_t> &pos) = 0;
+  virtual void sendPosition(uint8_t id, uint16_t pos) = 0;
+  virtual void on() = 0;
+  virtual void off() = 0;
+  virtual void setID(uint8_t currentID, uint8_t targetID) = 0;
+  virtual void setAllID(uint8_t targetID) = 0;
+  virtual void writeToROM(uint8_t id) = 0;
+  virtual void reboot(uint8_t id) = 0;
 };
 
 std::vector<uint8_t> makeFlagPacket(uint8_t id, uint8_t flag, uint8_t len);
--- central/test/mock/MockRSApi.h
+++ central/test/mock/MockRSApi.h
@@ -8,8 +8,12 @@ class MockRSApi : public RSApi
 {
   public:
     MOCK_METHOD2(sendMultiPosition, void(const std::vector<uint8_t> &, const std::vector<uint16_t> &));
-    MOCK_METHOD2(sendPosition, void(uint8_t id, uint16_t pos));
+    MOCK_METHOD2(sendPosition, void(uint8_t, uint16_t));
     MOCK_METHOD0(on, void(void));
     MOCK_METHOD0(off, void(void));
+    MOCK_METHOD2(setID, void(uint8_t, uint8_t));
+    MOCK_METHOD1(setAllID, void(uint8_t));
+    MOCK_METHOD1(writeToROM, void(uint8_t));
+    MOCK_METHOD1(reboot, void(uint8_t));
 };
 #endif
